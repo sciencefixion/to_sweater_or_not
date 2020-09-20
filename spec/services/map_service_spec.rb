798e9713729f9b7ea219eceee093e3ec138ba918
Rion Hutsell <st.gish@gmail.com>
2020-09-20 14:05:02 -0600

    Remove VCR from test to troubleshoot TravisCI failure

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Map Service' do
-  it 'retrieves latitude and longitude for a city', :vcr do
+  it 'retrieves latitude and longitude for a city' do
 
     latLng = {
     :lat => 39.738453,
