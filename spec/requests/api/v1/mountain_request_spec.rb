8863f1df293bcb665f104a8bc28fb92b0b353cb2
Rion Hutsell <st.gish@gmail.com>
2020-09-21 09:24:27 -0600

    Begin writing mountain request spec

---
+++ spec/requests/api/v1/mountain_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'Mountain request' do
+  it 'sends current forecast and nearby climbing_routes for a location and distance to each trail' do
+
+    get 'GET /api/v1/climbing_routes?location=erwin,tn'
+    routes = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.content_type).to eq('application/json')
+    # expect(forecast[:data]).to have_key(:id)
+    # expect(forecast[:data][:attributes]).to have_key(:current)
+    # expect(forecast[:data][:attributes]).to have_key(:hourly)
+    # expect(forecast[:data][:attributes]).to have_key(:timezone)
+    # expect(forecast[:data][:attributes]).to have_key(:daily)
+
+
+  end
+end
