dc1b94a7102a192f9e1beff490b2c72d8b65e37a
Rion Hutsell <st.gish@gmail.com>
2020-09-21 02:32:34 -0600

    Add forecast request spec

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -4,15 +4,16 @@ RSpec.describe 'Forecast request' do
   it 'sends weather data for a city' do
 
     get '/api/v1/forecast?location=denver,co'
+    forecast = JSON.parse(response.body, symbolize_names: true)
 
-    response = JSON.parse(response.body, symbolize_names: true)
     expect(response).to be_successful
-    expect(response).to have_key(:id)
-    expect(response).to have_key(:current)
-    expect(response).to have_key(:hourly)
-    expect(response).to have_key(:timezone)
-    expect(response).to have_key(:daily)
+    expect(response.content_type).to eq('application/json')
+    expect(forecast[:data]).to have_key(:id)
+    expect(forecast[:data][:attributes]).to have_key(:current)
+    expect(forecast[:data][:attributes]).to have_key(:hourly)
+    expect(forecast[:data][:attributes]).to have_key(:timezone)
+    expect(forecast[:data][:attributes]).to have_key(:daily)
 
-    
+
   end
 end
