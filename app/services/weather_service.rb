058b5e72d9e0617db7db1ba9aa39feaa2b92120e
Rion Hutsell <st.gish@gmail.com>
2020-09-20 19:38:47 -0600

    Add WeatherService and vcr cassettes for tests

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,21 +1,22 @@
 class WeatherService < BaseService
 
-  # 
-  # def self.get_forecast(lat, lon)
-  #   response = fore_conn.get('') do |req|
-  #     req.params[''] =
-  #     req.params[''] =
-  #   end
-  #   json(response)[:]
-  # end
-  #
-  # class << self
-  #   private
-  #
-  #   def fore_conn
-  #     conn('https://api.openweathermap.org') do |req|
-  #       req.params['key'] = ENV['OPEN_WEATHER_API_KEY']
-  #     end
-  #   end
-  # end
+
+  def self.get_forecast(lat, lon)
+    response = fore_conn.get('data/2.5/onecall') do |req|
+      req.params['lat'] = lat
+      req.params['lon'] = lon
+      req.params['exclude'] = 'minutely'
+    end
+    json(response)
+  end
+
+  class << self
+    private
+
+    def fore_conn
+      Faraday.new('https://api.openweathermap.org') do |req|
+        req.params['appid'] = ENV['OPEN_WEATHER_API_KEY']
+      end
+    end
+  end
 end
