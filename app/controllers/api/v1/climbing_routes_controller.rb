78fd3232607e33e3916c291ce81ad10f156f84d0
Rion Hutsell <st.gish@gmail.com>
2020-09-21 11:12:10 -0600

    Filter data in ClimbingRoutesController

--- app/controllers/api/v1/climbing_routes_controller.rb
+++ app/controllers/api/v1/climbing_routes_controller.rb
@@ -2,9 +2,22 @@ class Api::V1::ClimbingRoutesController < ApplicationController
   def index
     lat_lng = MapService.get_latitude_and_longitude(params[:location])
     forecast_data = WeatherService.get_forecast(lat_lng[:lat], lat_lng[:lng])
-    forecast = Forecast.new(forecast_data).current
-    x = MountainService.get_routes(lat_lng[:lat], lat_lng[:lng])
+    # forecast = Forecast.new(forecast_data).current
+    climbing_route_data = MountainService.get_routes(lat_lng[:lat], lat_lng[:lng])
+
+    nearby_routes = climbing_route_data[:routes]
+
+    filtered_routes = nearby_routes.each do |route|
+      route.except!(:stars, :starVotes, :pitches, )
+    end
+
+    filtered_routes.each do |route|
+      lon = route[:longitude]
+      lat = route[:latitude]
+      MapService.get_distance()
+    end
     binding.pry
-    # render json: MountainSerializer.new()
+    climbing_routes = ClimbingRoute.new(climbing_route_data, forecast_data)
+    # render json: ClimbingRouteSerializer.new()
   end
 end
