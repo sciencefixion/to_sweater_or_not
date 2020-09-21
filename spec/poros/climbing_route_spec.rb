949b36d4febf53796b86a39ed0bf0bcd60f662a8
Rion Hutsell <st.gish@gmail.com>
2020-09-21 10:26:57 -0600

    Begin writing ClimbingRoute poro spec

---
+++ spec/poros/climbing_route_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ClimbingRoute do
+  VCR.use_cassette('') do
+    it 'exists and has readable attributes' do
+
+      lat_lng = {
+      :lat => 39.738453,
+      :lng => -104.984853
+      }
+
+    
+    end
+  end
+end
