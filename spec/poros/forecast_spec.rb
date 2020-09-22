df087c4ef3f6c30797a49b12e218fe9a4c8af5b4
Rion Hutsell <st.gish@gmail.com>
2020-09-21 18:21:21 -0600

    Update Forecast poro spec and VCR cassettes

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -18,6 +18,21 @@ RSpec.describe Forecast do
       expect(forecast.current).to_not be_nil
       expect(forecast.hourly).to_not be_nil
       expect(forecast.daily).to_not be_nil
+      expect(forecast.current).to_not have_key(:pressure)
+      expect(forecast.current).to_not have_key(:dew_point)
+      expect(forecast.current).to_not have_key(:clouds)
+      expect(forecast.current).to_not have_key(:wind_speed)
+      expect(forecast.current).to_not have_key(:wind_gust)
+      expect(forecast.current).to_not have_key(:wind_deg)
+      expect(forecast.hourly.first).to_not have_key(:pressure)
+      expect(forecast.hourly.first).to_not have_key(:dew_point)
+      expect(forecast.hourly.first).to_not have_key(:clouds)
+      expect(forecast.hourly.first).to_not have_key(:wind_speed)
+      expect(forecast.hourly.first).to_not have_key(:wind_gust)
+      expect(forecast.hourly.first).to_not have_key(:pop)
+      expect(forecast.daily.first).to_not have_key(:pressure)
+      expect(forecast.daily.first[:temp]).to_not have_key(:eve)
+      expect(forecast.daily.first[:weather][0]).to_not have_key(:description)
     end
   end
 end
