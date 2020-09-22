462a2d90f676e8ce77d86bd6dd00df61106a65c2
Rion Hutsell <st.gish@gmail.com>
2020-09-22 01:31:42 -0600

    Add generate_api_key method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: { require: true }
+  validates_confirmation_of :password
+  # validates :api_key, presence: { require: true }, uniqueness: true
 
   has_secure_password
+
+  before_create :generate_api_key
+
+  def generate_api_key
+    self.api_key = SecureRandom.uuid
+  end
 end
