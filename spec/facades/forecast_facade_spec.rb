d82487e50ff230fe34f9f143d36cf14a973bea2f
Rion Hutsell <st.gish@gmail.com>
2020-09-22 17:24:53 -0600

    Update ForecastFacade to use VCR cassette

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,30 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe ForecastFacade do
-  it 'exists and does not include unnecessary data' do
+  VCR.use_cassette('sends_weather_data_for_a_city') do
+    it 'exists and does not include unnecessary data' do
 
-    location = 'Denver, CO'
+      location = 'Denver, CO'
 
-    forecast = ForecastFacade.get_forecast(location)
+      forecast = ForecastFacade.get_forecast(location)
 
-    expect(forecast.timezone).to_not be_nil
-    expect(forecast.current).to_not be_nil
-    expect(forecast.hourly).to_not be_nil
-    expect(forecast.daily).to_not be_nil
-    expect(forecast.current).to_not have_key(:pressure)
-    expect(forecast.current).to_not have_key(:dew_point)
-    expect(forecast.current).to_not have_key(:clouds)
-    expect(forecast.current).to_not have_key(:wind_speed)
-    expect(forecast.current).to_not have_key(:wind_gust)
-    expect(forecast.current).to_not have_key(:wind_deg)
-    expect(forecast.hourly.first).to_not have_key(:pressure)
-    expect(forecast.hourly.first).to_not have_key(:dew_point)
-    expect(forecast.hourly.first).to_not have_key(:clouds)
-    expect(forecast.hourly.first).to_not have_key(:wind_speed)
-    expect(forecast.hourly.first).to_not have_key(:wind_gust)
-    expect(forecast.hourly.first).to_not have_key(:pop)
-    expect(forecast.daily.first).to_not have_key(:pressure)
-    expect(forecast.daily.first[:temp]).to_not have_key(:eve)
-    expect(forecast.daily.first[:weather][0]).to_not have_key(:description)
+      expect(forecast.timezone).to_not be_nil
+      expect(forecast.current).to_not be_nil
+      expect(forecast.hourly).to_not be_nil
+      expect(forecast.daily).to_not be_nil
+      expect(forecast.current).to_not have_key(:pressure)
+      expect(forecast.current).to_not have_key(:dew_point)
+      expect(forecast.current).to_not have_key(:clouds)
+      expect(forecast.current).to_not have_key(:wind_speed)
+      expect(forecast.current).to_not have_key(:wind_gust)
+      expect(forecast.current).to_not have_key(:wind_deg)
+      expect(forecast.hourly.first).to_not have_key(:pressure)
+      expect(forecast.hourly.first).to_not have_key(:dew_point)
+      expect(forecast.hourly.first).to_not have_key(:clouds)
+      expect(forecast.hourly.first).to_not have_key(:wind_speed)
+      expect(forecast.hourly.first).to_not have_key(:wind_gust)
+      expect(forecast.hourly.first).to_not have_key(:pop)
+      expect(forecast.daily.first).to_not have_key(:pressure)
+      expect(forecast.daily.first[:temp]).to_not have_key(:eve)
+      expect(forecast.daily.first[:weather][0]).to_not have_key(:description)
+    end
   end
 end
