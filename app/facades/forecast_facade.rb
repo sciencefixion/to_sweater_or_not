6a01a9be5500fac410947c1255f6300e2bfa039a
Rion Hutsell <st.gish@gmail.com>
2020-09-22 11:49:58 -0600

    Remove whitespace

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -2,10 +2,8 @@ class ForecastFacade
 
   def self.current_summary(lat_lng)
     forecast_data = WeatherService.get_forecast(lat_lng[:lat], lat_lng[:lng])
-
     current_forecast = Forecast.new(forecast_data).current
-
-    forecast_hash = {
+    {
       'summary': "#{current_forecast[:weather][0][:description]}",
       'temperature': "#{current_forecast[:temp]}",
     }
