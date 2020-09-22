4f715c8fd3bc7a51a25fc3dfcd1340eaff65fec6
Rion Hutsell <st.gish@gmail.com>
2020-09-21 21:45:14 -0600

    Add users create route and add VCR to forecast request spec

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Forecast request' do
-  it 'sends weather data for a city' do
+  it 'sends weather data for a city', :vcr do
 
     get '/api/v1/forecast?location=denver,co'
     forecast = JSON.parse(response.body, symbolize_names: true)
