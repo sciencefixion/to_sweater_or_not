22f4b0675e2dfb8701f08af404a4703c28e839b4
Rion Hutsell <st.gish@gmail.com>
2020-09-23 08:33:40 -0600

    Update login and user registration specs with headers

--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'User Registration request' do
   it 'allows a user to create an account' do
-    user_params = {
+    body = {
       email: 'walter@sobchak_security.com',
       password: 'AWorldOfPain',
       password_confirmation: 'AWorldOfPain'
     }
 
-    post '/api/v1/users', params: user_params
+    post '/api/v1/users', params: body.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
     registration_response = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
@@ -18,17 +18,17 @@ RSpec.describe 'User Registration request' do
     expect(registration_response).to_not have_key(:password)
     expect(registration_response[:data][:attributes]).to have_key(:api_key)
     expect(registration_response[:data][:attributes]).to have_key(:email)
-    expect(registration_response[:data][:attributes][:email]).to eq(user_params[:email])
+    expect(registration_response[:data][:attributes][:email]).to eq(body[:email])
     expect(registration_response[:data][:attributes][:api_key]).to_not be_nil
   end
   it 'fails with status 400 when passwords do not match' do
-    user_params = {
+    body = {
       email: 'walter@sobchak_security.com',
       password: 'AWorldOfPain',
       password_confirmation: 'ICanGetYouAToeBy3pmWithNailPolish'
     }
 
-    post '/api/v1/users', params: user_params
+    post '/api/v1/users', params: body.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
 
     registration_response = JSON.parse(response.body, symbolize_names: true)
 
@@ -39,14 +39,14 @@ RSpec.describe 'User Registration request' do
     expect(registration_response).to_not have_key(:password)
   end
   it 'fails with status 400 when email has already been taken' do
-    user_params = {
+    body = {
       email: 'walter@sobchak_security.com',
       password: 'AWorldOfPain',
       password_confirmation: 'AWorldOfPain'
     }
-    walter = User.create(user_params)
+    walter = User.create(body)
 
-    post '/api/v1/users', params: user_params
+    post '/api/v1/users', params: body.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
 
     registration_response = JSON.parse(response.body, symbolize_names: true)
 
@@ -57,12 +57,12 @@ RSpec.describe 'User Registration request' do
     expect(registration_response).to_not have_key(:password)
   end
   it 'fails with status 400 when there is a missing field' do
-    user_params = {
+    body = {
       email: 'walter@sobchak_security.com',
       password: 'AWorldOfPain'
     }
 
-    post '/api/v1/users', params: user_params
+    post '/api/v1/users', params: body.to_json, headers: { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
 
     registration_response = JSON.parse(response.body, symbolize_names: true)
     expect(response).to_not be_successful
