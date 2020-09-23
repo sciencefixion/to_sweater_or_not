0e4c936a4897cdb8f1aafa07a23ab524a3e16bd1
Rion Hutsell <st.gish@gmail.com>
2020-09-23 00:45:15 -0600

    Update Road Trip request spec

--- spec/requests/api/v1/road_trip_request_spec.rb
+++ spec/requests/api/v1/road_trip_request_spec.rb
@@ -18,18 +18,19 @@ RSpec.describe 'Road Trip request' do
 
       post '/api/v1/road_trip', params: body
 
+      trip = JSON.parse(response.body, symbolize_names: true)
 
       expect(response).to be_successful
       expect(response.status).to eq(201)
       expect(response.content_type).to eq('application/json')
-      expect(forecast[:data]).to have_key(:id)
-      expect(forecast[:data][:attributes]).to have_key(:origin)
-      expect(forecast[:data][:attributes]).to have_key(:destination)
-      expect(forecast[:data][:attributes]).to have_key(:travel_time)
-      expect(forecast[:data][:attributes]).to have_key(:arrival_forecast)
+      expect(trip[:data]).to have_key(:id)
+      expect(trip[:data][:attributes]).to have_key(:origin)
+      expect(trip[:data][:attributes]).to have_key(:destination)
+      expect(trip[:data][:attributes]).to have_key(:travel_time)
+      expect(trip[:data][:attributes]).to have_key(:arrival_forecast)
     end
   end
-  xit 'fails with status 401 with no api_key provided' do
+  it 'fails with status 401 with no api_key provided' do
 
     body = {
       origin: 'Denver,CO',
@@ -38,12 +39,12 @@ RSpec.describe 'Road Trip request' do
 
     post '/api/v1/road_trip', params: body
 
-    registration_response = JSON.parse(response.body, symbolize_names: true)
+    error_response = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to_not be_successful
     expect(response.status).to eq(401)
     expect(response.content_type).to eq('application/json')
-    expect(registration_response[:body]).to include('Bad credentials')
-    expect(registration_response).to_not have_key(:password)
+    expect(error_response[:body]).to include('Unauthorized')
+    expect(error_response).to_not have_key(:password)
   end
 end
